0c48f80d16a843e3a6178e4bb445125c7bf44319
l18desuq <leandro-huemul.desuque@imt-atlantique.net>
2019-04-30 16:32:45 +0200

    Grafica funcionando

--- generateGraphPartition.py
+++ generateGraphPartition.py
@@ -8,6 +8,8 @@ Created on Tue Apr 30 10:57:11 2019
 
 import networkx as nx
 import metis
+import matplotlib.pyplot as plt
+from matplotlib import pylab
 #from cStringIO import StringIO
 
 """
@@ -36,15 +38,94 @@ nx.nx_pydot.write_dot(G, 'example.dot')
 
 class GenerateGraphPartition():
     
-    def createEdgesGraph():
+    def createEdgesGraph(self):
         print("hola")
         graph = nx.read_gexf("mediumLinkedin.gexf")
         print("hola")
         print(graph.edges())
         print(graph.nodes())
-        (cut, parts) = metis.part_graph(graph, 2)
+        (cut, parts) = metis.part_graph(graph, 40)
+
+
 
         print(parts)
+        self.draw_graph(graph, parts)
+
+    def draw_graph(self, g, parts, node_attribute=None, list_of_values_of_attributes=None):
+        """
+        Draw the graph g.
+
+        Parameters
+        ----------
+        g : graph
+           A networkx graph
+        node_attribute : string
+           The name of the node attribute used to assign colors to the drawing
+        list_of_values_of_attributes : list
+            A list of all the potential values of node_attribute to assign one color
+            per value.
+        """
+        # initialze Figure
+        plt.figure(num=None, figsize=(80, 80), dpi=80)
+        plt.axis('off')
+        fig = plt.figure(1)
+
+        pos = nx.spring_layout(g, iterations=100)
+
+        ### beta
+        values = []
+        i = 0
+        for node in g.nodes():
+            #if parts[i] == 1:
+                    # we arbitrarily take the first value
+                   # values.append(2)
+            #else:
+            values.append(parts[i])
+            i += 1
+
+        nx.draw_networkx_nodes(g, pos, cmap=plt.get_cmap('jet'), node_color=values)
+        ####
+        """
+        if node_attribute and list_of_values_of_attributes:
+            # To associate colors to nodes according to an attribute, here college
+            # build a color_map, one for each college
+            color_map = {}
+            i = 0.0
+            for s in list_of_values_of_attributes:
+                color_map[s] = i
+                i += 1 / len(list_of_values_of_attributes)
+            color_map[None] = 1  # for nodes without values for the attribute node_attribute
+
+            # The values supplied to node_color should be in the same order as the nodes
+            # listed in G.nodes(). We take an arbitrary mapping of values color_map and
+            # generate the values list in the correct order
+            # values = [color_map[G.node[node].get(node_attribute)] for node in G.nodes()] # for attributes encoded in the graph
+            values = []
+            for node in g.nodes():
+                if node in node_attribute:
+                    if node_attribute[node]:
+                        # we arbitrarily take the first value
+                        values.append(color_map[node_attribute[node][0]])
+                else:
+                    values.append(1)
+
+            nx.draw_networkx_nodes(g, pos, cmap=plt.get_cmap('jet'), node_color=values)
+        else:
+            nx.draw_networkx_nodes(g, pos)
+        """
+
+        nx.draw_networkx_edges(g, pos)
+        nx.draw_networkx_labels(g, pos)
+
+        cut = 1.00
+        #xmax = cut * max(xx for xx, yy in pos.values())
+        #ymax = cut * max(yy for xx, yy in pos.values())
+        #plt.xlim(0, xmax)
+        #plt.ylim(0, ymax)
+        plt.show()
+        plt.savefig('foo.png')
+        pylab.close()
+        del fig
 """
     def plotGraph(pydot_graph):
         
--- mainTest.py
+++ mainTest.py
@@ -5,12 +5,15 @@ Created on Tue Apr 30 11:08:56 2019
 
 @author: g18quint
 """
-from generateGraphPartition import GenerateGraphPartition as ggp
+from generateGraphPartition import GenerateGraphPartition
+
 
 def mainTest():
     print("hola")
+    ggp = GenerateGraphPartition()
     ggp.createEdgesGraph()
     print("hola")
-    
+
+
 if __name__ == '__main__':
     mainTest()
